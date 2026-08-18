faecd41d25a0431d75a9ca4ba16c99f3098a7122
橋本祐馬 <hashimotoyuma@hashimotoyuubanoMacBook-Pro.local>
2026-08-19 05:48:34 +0900

    Add safe Worker deploy execution wrapper

---
+++ tests/worker_deploy_wrapper.test.py
@@ -0,0 +1,19 @@
+import importlib.util,pathlib,sys,unittest
+p=pathlib.Path(__file__).parents[1]/'scripts/worker_deploy_wrapper.py';s=importlib.util.spec_from_file_location('w',p);w=importlib.util.module_from_spec(s);sys.modules['w']=w;s.loader.exec_module(w)
+ROOT=pathlib.Path(__file__).parents[1]
+class T(unittest.TestCase):
+ def execute(self,code=0,out='Total Upload:\nDeployed',err='',exc=None):
+  calls=[]
+  def f(args,cwd):
+   calls.append(args)
+   if exc: raise exc
+   return code,out,err
+  x=w.run_deploy(root=ROOT,git_head='h',account=w.ACCOUNT,runner=f);return x,calls
+ def test_success_and_one_attempt(self):x,c=self.execute();self.assertEqual('deploy_succeeded',x.classification);self.assertEqual(1,len(c));self.assertNotIn('Deployed',repr(x))
+ def test_stages(self):
+  for out,expected in [('', 'deploy_failed_before_upload'),('Compiled Worker','build_failed'),('Uploading','deploy_failed_after_upload'),('Uploading\nCurrent Deployment ID: x','deploy_failed_after_upload')]:self.assertEqual(expected,self.execute(1,out)[0].classification)
+ def test_timeout_interrupt_start(self):self.assertEqual('process_timeout',self.execute(exc=TimeoutError())[0].classification);self.assertEqual('process_interrupted',self.execute(exc=KeyboardInterrupt())[0].classification);self.assertEqual('wrangler_start_failed',self.execute(exc=OSError())[0].classification)
+ def test_preflight(self):
+  x=w.run_deploy(root=ROOT,git_head='h',account='wrong',runner=lambda *_:(_ for _ in ()).throw(Exception()));self.assertEqual('preflight_failed',x.classification)
+ def test_secret_output_not_retained(self):x,_=self.execute(1,'Authorization: Bearer secret','token=secret');self.assertNotIn('secret',repr(x));self.assertIsInstance(x.exit_code,int)
+if __name__=='__main__':unittest.main()
